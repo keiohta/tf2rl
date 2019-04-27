f846c86a63b65efc93b578e5704552223ad00f99
Yamada <>
2019-04-27 20:25:48 +0900

    Add setup.py and __init__.py for installation
    Fix #1

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,Extension,find_packages
+
+setup(name = "tf2rl",
+      install_requires = ["cpprb","tensorflow"],
+      url = "https://github.com/keiohta/tf2rl",
+      packages = find_packages("."),
+      extras_require = {"examples" : ["gym","roboschool"]})
