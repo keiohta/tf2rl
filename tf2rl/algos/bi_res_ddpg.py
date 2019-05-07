f80823ae44669cce3c8fe591e1270e2dcdbc0e85
Kei Ohta <dev.ohtakei@gmail.com>
2019-05-07 22:17:16 +0900

    Add Bi-Res-DDPG agent

---
+++ tf2rl/algos/bi_res_ddpg.py
@@ -0,0 +1,56 @@
+import numpy as np
+import tensorflow as tf
+
+from tf2rl.algos.ddpg import DDPG
+from tf2rl.misc.target_update_ops import update_target_variables
+
+
+class BiResDDPG(DDPG):
+    def __init__(self, eta=0.05, **kwargs):
+        super().__init__(**kwargs)
+        self._eta = eta
+
+    @tf.contrib.eager.defun
+    def _train_body(self, states, actions, next_states, rewards, done, weights):
+        with tf.device(self.device):
+            with tf.GradientTape() as tape:
+                td_errors1, td_errors2 = self._compute_td_error_body(
+                    states, actions, next_states, rewards, done)
+                critic_loss = tf.reduce_mean(
+                    tf.square(td_errors1 * weights) * 0.5 + \
+                    tf.square(td_errors2 * weights * self.discount * self._eta) * 0.5)
+
+            critic_grad = tape.gradient(critic_loss, self.critic.trainable_variables)
+            self.critic_optimizer.apply_gradients(zip(critic_grad, self.critic.trainable_variables))
+
+            with tf.GradientTape() as tape:
+                next_action = self.actor(states)
+                actor_loss = -tf.reduce_mean(self.critic([states, next_action]))
+
+            actor_grad = tape.gradient(actor_loss, self.actor.trainable_variables)
+            self.actor_optimizer.apply_gradients(zip(actor_grad, self.actor.trainable_variables))
+
+            # Update target networks
+            update_target_variables(self.critic_target.weights, self.critic.weights, self.tau)
+            update_target_variables(self.actor_target.weights, self.actor.weights, self.tau)
+
+            return actor_loss, critic_loss, np.abs(td_errors1) + np.abs(td_errors2)
+
+    @tf.contrib.eager.defun
+    def _compute_td_error_body(self, states, actions, next_states, rewards, done):
+        with tf.device(self.device):
+            not_done = 1. - done
+            # Compute standard TD error
+            target_Q = self.critic_target(
+                [next_states, self.actor_target(next_states)])
+            target_Q = rewards + (not_done * self.discount * target_Q)
+            target_Q = tf.stop_gradient(target_Q)
+            current_Q = self.critic([states, actions])
+            td_errors1 = target_Q - current_Q
+            # Compute residual TD error
+            next_actions = tf.stop_gradient(self.actor(next_states))
+            target_Q = self.critic([next_states, next_actions])
+            target_Q = rewards + (not_done * self.discount * target_Q)
+            current_Q = tf.stop_gradient(self.critic_target([states, actions]))
+            td_errors2 = target_Q - current_Q
+        return td_errors1, td_errors2
