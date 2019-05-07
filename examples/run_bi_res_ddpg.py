f04313558d28b6460f0267156473da06f17bfbf3
Kei Ohta <dev.ohtakei@gmail.com>
2019-05-07 22:17:16 +0900

    Add Bi-Res-DDPG agent

---
+++ examples/run_bi_res_ddpg.py
@@ -0,0 +1,22 @@
+import roboschool, gym
+
+from tf2rl.algos.bi_res_ddpg import BiResDDPG
+from tf2rl.trainer.trainer import Trainer
+
+
+if __name__ == '__main__':
+    parser = Trainer.get_argument()
+    parser.add_argument('--env-name', type=str, default="RoboschoolAnt-v1")
+    parser.add_argument('--eta', type=float, default=0.05)
+    args = parser.parse_args()
+
+    env = gym.make(args.env_name)
+    test_env = gym.make(args.env_name)
+    policy = BiResDDPG(
+        state_dim=env.observation_space.high.size,
+        action_dim=env.action_space.high.size,
+        gpu=args.gpu,
+        eta=args.eta,
+        batch_size=100)
+    trainer = Trainer(policy, env, args, test_env=test_env)
+    trainer()
