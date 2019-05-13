e81c7345a11daeac270150c6bf7f323feaa15ec4
Kei Ohta <dev.ohtakei@gmail.com>
2019-05-13 23:26:38 +0900

    Fix a bug of computing critic loss

--- tf2rl/algos/bi_res_ddpg.py
+++ tf2rl/algos/bi_res_ddpg.py
@@ -22,8 +22,8 @@ class BiResDDPG(DDPG):
                 td_errors1, td_errors2 = self._compute_td_error_body(
                     states, actions, next_states, rewards, done)
                 critic_loss = tf.reduce_mean(
-                    tf.square(td_errors1 * weights) * 0.5 + \
-                    tf.square(td_errors2 * weights * self.discount * self._eta) * 0.5)
+                    tf.square(td_errors1) * weights * 0.5 + \
+                    tf.square(td_errors2) * weights * self.discount * self._eta * 0.5)
 
             critic_grad = tape.gradient(critic_loss, self.critic.trainable_variables)
             self.critic_optimizer.apply_gradients(zip(critic_grad, self.critic.trainable_variables))
