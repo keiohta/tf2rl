8464b10a3c793a883a068f791c1bd379cf1654b3
Kei Ohta <dev.ohtakei@gmail.com>
2019-05-15 22:26:50 +0900

    Improve readability but remain shape bug

--- tf2rl/algos/categorical_dqn.py
+++ tf2rl/algos/categorical_dqn.py
@@ -24,15 +24,14 @@ class CategoricalQFunc(QFunc):
             features = tf.nn.relu(self.l3(features))
             features = tf.reshape(features, (-1, self._action_dim, self._n_atoms))
             return tf.clip_by_value(
-                tf.keras.activations.softmax(features, axis=2),
-                1e-8, 1.0-1e-8)
+                tf.keras.activations.softmax(features, axis=2), 1e-8, 1.0-1e-8)
 
 
 class CategoricalDQN(DQN):
     def __init__(self, *args, **kwargs):
         kwargs["q_func"] = CategoricalQFunc
         super().__init__(*args, **kwargs)
-        self._v_max, self._v_min = 20., 0.
+        self._v_max, self._v_min = 10., -10.
         self._delta_z = (self._v_max - self._v_min) / (self.q_func._n_atoms - 1)
         self._z_list = tf.constant(
             [self._v_min + i * self._delta_z for i in range(self.q_func._n_atoms)],
@@ -51,9 +50,10 @@ class CategoricalDQN(DQN):
         else:
             state = np.expand_dims(state, axis=0).astype(np.float64)
             action_probs = self._get_action_body(tf.constant(state))
-            # TODO: reduce_mean?
             action = tf.argmax(
-                tf.reduce_sum(action_probs * self.z_list_broadcasted, axis=2), axis=1).numpy()[0]
+                tf.reduce_sum(action_probs * self._z_list_broadcasted, axis=2),
+                axis=1)
+            action = action.numpy()[0]
 
         return action
 
@@ -74,7 +74,7 @@ class CategoricalDQN(DQN):
     def _compute_td_error_body(self, states, actions, next_states, rewards, done):
         actions = tf.cast(actions, dtype=tf.int32)
         with tf.device(self.device):
-            indices = tf.concat(
+            action_indices = tf.concat(
                 values=[tf.expand_dims(tf.range(self.batch_size), axis=1),
                         actions], axis=1)
 
@@ -83,32 +83,30 @@ class CategoricalDQN(DQN):
             else:
                 rewards = tf.tile(
                     tf.reshape(rewards, [-1, 1]),
-                    tf.constant([1, self.q_func._n_atoms]))
+                    tf.constant([1, self.q_func._n_atoms]))  # [batch_size, n_atoms]
                 not_done = 1.0 - tf.tile(
                     tf.reshape(done ,[-1, 1]),
-                    tf.constant([1, self.q_func._n_atoms]))
+                    tf.constant([1, self.q_func._n_atoms]))  # [batch_size, n_atoms]
                 discounts = tf.cast(
                     tf.reshape(self.discount, [-1, 1]), tf.float64)
-                target_Q = tf.reshape(
-                    self._z_list, [1, self.q_func._n_atoms])
-                target_Q = rewards + not_done * discounts * target_Q 
-                b = (target_Q - self._v_min) / self._delta_z
+                z = tf.reshape(
+                    self._z_list, [1, self.q_func._n_atoms])  # [1, n_atoms]
+                z = rewards + not_done * discounts * z  # [batch_size, n_atoms]
+                b = (z - self._v_min) / self._delta_z  # [batch_size, n_atoms]
 
                 index_help = tf.expand_dims(
                     tf.tile(
                         tf.reshape(tf.range(self.batch_size), [-1, 1]),
                         tf.constant([1, self.q_func._n_atoms])),
-                    -1)
-                u, l = tf.ceil(b), tf.floor(b)
+                    -1)  # [batch_size, n_atoms, 1]
+                u, l = tf.ceil(b), tf.floor(b)  # [batch_size, n_atoms]
                 u_id = tf.concat(
                     [index_help, tf.expand_dims(tf.cast(u, tf.int32), -1)],
-                    axis=2)
+                    axis=2)  # [batch_size, n_atoms, 2]
                 l_id = tf.concat(
                     [index_help, tf.expand_dims(tf.cast(l, tf.int32), -1)],
-                    axis=2)
+                    axis=2)  # [batch_size, n_atoms, 2]
 
-                current_Q = tf.gather_nd(
-                    self.q_func(states), indices)
                 target_Q_next = self.q_func_target(next_states)
                 target_Q_next_sum = tf.reduce_sum(
                     target_Q_next * self._z_list_broadcasted, axis=2)
@@ -122,9 +120,12 @@ class CategoricalDQN(DQN):
                          tf.reshape(actions_by_target_Q, [-1, 1])],
                         axis=1))
 
+                current_Q = tf.gather_nd(
+                    self.q_func(states), action_indices)
+
                 td_errors = tf.reduce_sum(
-                    current_Q * (u - b) * tf.log(tf.gather_nd(current_Q, l_id)) + \
-                    current_Q * (b - l) * tf.log(tf.gather_nd(current_Q, u_id)),
+                    target_Q_next * (u - b) * tf.log(tf.gather_nd(current_Q, l_id)) + \
+                    target_Q_next * (b - l) * tf.log(tf.gather_nd(current_Q, u_id)),
                     axis=1)
 
         return td_errors
