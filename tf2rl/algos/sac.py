5a432a7ba7785f6fb3365c8799823f2b3d4f248c
Kei Ohta <dev.ohtakei@gmail.com>
2019-05-18 15:11:39 -0500

    Add tf2rl/algos/models.py to reuse models between different algos

--- tf2rl/algos/sac.py
+++ tf2rl/algos/sac.py
@@ -2,61 +2,10 @@ import numpy as np
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
-from tensorflow.contrib.distributions import MultivariateNormalDiag
 
+from tf2rl.algos.models import GaussianActor
 from tf2rl.algos.policy_base import OffPolicyAgent
 from tf2rl.misc.target_update_ops import update_target_variables
-
-
-class GaussianActor(tf.keras.Model):
-    LOG_SIG_CAP_MAX = 2
-    LOG_SIG_CAP_MIN = -20
-    EPS = 1e-6
-
-    def __init__(self, state_dim, action_dim, max_action, units=[256, 256],
-                 name='GaussianPolicy'):
-        super().__init__(name=name)
-
-        self.l1 = Dense(units[0], name="L1", activation='relu')
-        self.l2 = Dense(units[1], name="L2", activation='relu')
-        self.out_mean = Dense(action_dim, name="L_mean")
-        self.out_sigma = Dense(action_dim, name="L_sigma")
-
-        self._max_action = max_action
-
-        dummy_state = tf.constant(np.zeros(shape=[1, state_dim], dtype=np.float64))
-        self(dummy_state)
-
-    def _compute_dist(self, states):
-        features = self.l1(states)
-        features = self.l2(features)
-
-        mu = self.out_mean(features)
-        log_sigma = self.out_sigma(features)
-        log_sigma = tf.clip_by_value(log_sigma, self.LOG_SIG_CAP_MIN, self.LOG_SIG_CAP_MAX)
-
-        return MultivariateNormalDiag(loc=mu, scale_diag=tf.exp(log_sigma))
-
-    def call(self, states):
-        dist = self._compute_dist(states)
-        raw_actions = dist.sample()
-        log_pis = dist.log_prob(raw_actions)
-
-        actions = tf.tanh(raw_actions)
-
-        # for variable replacement
-        diff = tf.reduce_sum(tf.log(1. - actions ** 2 + self.EPS), axis=1)
-        log_pis -= diff
-
-        actions = actions * self._max_action
-        return actions, log_pis
-
-    def mean_action(self, states):
-        dist = self._compute_dist(states)
-        raw_actions = dist.mean()
-        actions = tf.tanh(raw_actions) * self._max_action
-
-        return actions
 
 
 class CriticV(tf.keras.Model):
