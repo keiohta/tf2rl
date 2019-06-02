d62628af389fb397cc2e14698014ea527942582f
Kei Ohta <dev.ohtakei@gmail.com>
2019-06-02 18:01:47 -0400

    Experimentally add common class for tests of algos

---
+++ tests/algos/common.py
@@ -0,0 +1,14 @@
+import unittest
+import gym
+import numpy as np
+import tensorflow as tf
+
+from tf2rl.algos.dqn import DQN
+
+
+class CommonAlgos(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.env = gym.make("CartPole-v0")
+        cls.continuous_env = gym.make("Pendulum-v0")
+        cls.batch_size = 32
