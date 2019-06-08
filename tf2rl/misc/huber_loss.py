f40e5a360d876265b8f4dc821936df81fbed0cf7
Kei Ohta <dev.ohtakei@gmail.com>
2019-06-08 17:23:32 -0400

    Add test for huber loss

--- tf2rl/misc/huber_loss.py
+++ tf2rl/misc/huber_loss.py
@@ -20,4 +20,7 @@ def huber_loss(y_target=None, y_pred=None, diff=None, max_grad=1.):
         diff = tf.abs(y_target - y_pred)
     less_than_max = 0.5 * tf.square(diff)
     greater_than_max = max_grad * (diff - 0.5 * max_grad)
-    return tf.where(diff <= max_grad, x=less_than_max, y=greater_than_max)
+    return tf.where(
+        diff <= max_grad,
+        x=less_than_max,
+        y=greater_than_max)
