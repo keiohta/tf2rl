332a489e60386e615271a084c88fb13a6edd350f
Kei Ohta <dev.ohtakei@gmail.com>
2019-06-08 13:59:19 -0400

    Add a test to verify compute_td_erro

--- tests/algos/common.py
+++ tests/algos/common.py
@@ -15,16 +15,15 @@ class CommonAlgos(unittest.TestCase):
 
 class CommonContinuousOutputAlgos(CommonAlgos):
     def test_get_action(self):
-        state = self.continuous_env.reset()
         if self.agent is None:
             return
+        state = self.continuous_env.reset()
         self.agent.get_action(state, test=False)
         self.agent.get_action(state, test=True)
 
     def test_train(self):
         if self.agent is None:
             return
-
         rewards = np.zeros(shape=(self.batch_size,1), dtype=np.float32)
         dones = np.zeros(shape=(self.batch_size,1), dtype=np.float32)
         obses = np.zeros(
@@ -35,6 +34,21 @@ class CommonContinuousOutputAlgos(CommonAlgos):
             dtype=np.float32)
         self.agent.train(
             obses, acts, obses, rewards, dones)
+
+    def test_compute_td_error(self):
+        if self.agent is None:
+            return
+        rewards = np.zeros(shape=(self.batch_size,1), dtype=np.float32)
+        dones = np.zeros(shape=(self.batch_size,1), dtype=np.float32)
+        obses = np.zeros(
+            shape=(self.batch_size,)+self.continuous_env.observation_space.shape,
+            dtype=np.float32)
+        acts = np.zeros(
+            shape=(self.batch_size,self.continuous_env.action_space.low.size,),
+            dtype=np.float32)
+        self.agent.compute_td_error(
+            states=obses, actions=acts, next_states=obses,
+            rewards=rewards, dones=dones)
 
 
 class CommonDiscreteOutputAlgos(CommonAlgos):
