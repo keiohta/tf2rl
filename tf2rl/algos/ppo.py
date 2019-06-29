c52a33b155f0eef88803ee34e9092aaa5a7da8bb
Kei Ohta <dev.ohtakei@gmail.com>
2019-06-29 22:32:23 +0900

    Add PPO agent

---
+++ tf2rl/algos/ppo.py
@@ -0,0 +1,61 @@
+import numpy as np
+import tensorflow as tf
+
+from tf2rl.algos.vpg import VPG
+
+
+class PPO(VPG):
+    def __init__(
+            self,
+            clip=True,
+            clip_ratio=0.2,
+            name="PPO",
+            **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.clip = clip
+        self.clip_ratio = clip_ratio
+
+    def train_actor(self, states, actions, advantages, logp_olds):
+        actor_loss, logp_news, ratio = self._train_actor_body(
+            states, actions, advantages, logp_olds)
+        tf.summary.scalar(name=self.policy_name+"/actor_loss", data=actor_loss)
+        tf.summary.scalar(name=self.policy_name+"/logp_max",
+                          data=np.max(logp_news))
+        tf.summary.scalar(name=self.policy_name+"/logp_min",
+                          data=np.min(logp_news))
+        tf.summary.scalar(name=self.policy_name+"/logp_mean",
+                          data=np.mean(logp_news))
+        tf.summary.scalar(name=self.policy_name+"/adv_max",
+                          data=np.max(advantages))
+        tf.summary.scalar(name=self.policy_name+"/adv_min",
+                          data=np.min(advantages))
+        # TODO: Compute KL divergence and output it
+        approx_kl = tf.reduce_mean(logp_olds - logp_news)
+        approx_ent = tf.reduce_mean(-logp_news)
+        tf.summary.scalar(name=self.policy_name+"/kl", data=approx_kl)
+        tf.summary.scalar(name=self.policy_name+"/ent", data=approx_ent)
+        return actor_loss
+
+    @tf.function
+    def _train_actor_body(self, states, actions, advantages, logp_olds):
+        with tf.device(self.device):
+            # Train policy
+            with tf.GradientTape() as tape:
+                if self.clip:
+                    logp_news = self.actor.compute_log_probs(states, actions)
+                    ratio = tf.math.exp(logp_news - tf.squeeze(logp_olds))
+                    min_adv = tf.squeeze(tf.where(
+                        advantages > 0,
+                        (1. + self.clip_ratio) * advantages,
+                        (1. - self.clip_ratio) * advantages))
+                    surr_loss = - \
+                        tf.minimum(ratio * tf.squeeze(advantages), min_adv)
+                else:
+                    raise NotImplementedError
+                actor_loss = tf.reduce_mean(surr_loss)  # + lambda * entropy
+            actor_grad = tape.gradient(
+                actor_loss, self.actor.trainable_variables)
+            self.actor_optimizer.apply_gradients(
+                zip(actor_grad, self.actor.trainable_variables))
+
+        return actor_loss, logp_news, ratio
