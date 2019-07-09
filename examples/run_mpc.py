035257c6ec70a7d803c66eeb60bfd674e50533f2
Kei Ohta <dev.ohtakei@gmail.com>
2019-07-09 22:22:22 +0900

    Add MPC #45

---
+++ examples/run_mpc.py
@@ -0,0 +1,29 @@
+import numpy as np
+import gym
+
+from tf2rl.experiments.mpc_trainer import MPCTrainer
+
+
+if __name__ == "__main__":
+    parser = MPCTrainer.get_argument()
+    parser.set_defaults(episode_max_steps=200)
+    args = parser.parse_args()
+
+    env = gym.make("Pendulum-v0")
+
+    def reward_fn(obses, next_obses, acts):
+        assert obses.ndim == next_obses.ndim == acts.ndim == 2
+        assert obses.shape[0] == next_obses.shape[0] == acts.shape[0]
+        acts = np.squeeze(acts)
+        thetas = obses[:, 0]
+        theta_dots = obses[:, 1]
+
+        def angle_normalize(x):
+            return (((x+np.pi) % (2*np.pi)) - np.pi)
+
+        acts = np.clip(acts, -2, 2)
+        costs = angle_normalize(thetas)**2 + .1*theta_dots**2 + .001*(acts**2)
+        return -costs
+
+    trainer = MPCTrainer(env, args, reward_fn)
+    trainer()
