a8510ae89dc18aaa720b51903825de3126a2e460
Kei Ohta <dev.ohtakei@gmail.com>
2019-11-27 19:15:00 +0900

    Fix a bug of computing action in DDPG #56

--- tf2rl/algos/ddpg.py
+++ tf2rl/algos/ddpg.py
@@ -97,7 +97,8 @@ class DDPG(OffPolicyAgent):
         state = np.expand_dims(state, axis=0).astype(
             np.float32) if is_single_state else state
         action = self._get_action_body(
-            tf.constant(state), self.sigma * test, tf.constant(self.actor.max_action, dtype=tf.float32))
+            tf.constant(state), self.sigma * (1. - test),
+            tf.constant(self.actor.max_action, dtype=tf.float32))
         if tensor:
             return action
         else:
