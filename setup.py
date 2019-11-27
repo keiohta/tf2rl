5d2f3250bb88f9711a5ae2ec6d1895f4c9aa27fc
Kei Ohta <dev.ohtakei@gmail.com>
2019-11-27 21:33:09 +0900

    Set current version 0.1.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extras_require = {
 
 setup(
     name="tf2rl",
-    version="0.1.4",
+    version="0.1.5",
     description="Deep Reinforcement Learning for TensorFlow2.0",
     url="https://github.com/keiohta/tf2rl",
     author="Kei Ohta",
