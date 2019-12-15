60579c58a02bfcb25fed6ced2f4b20db5c9566a1
Yamada <>
2019-12-15 20:06:55 +0900

    Separate _dummy_call from __init__. Rename Network to StateNetwork

--- tf2rl/networks/network_base.py
+++ tf2rl/networks/network_base.py
@@ -2,8 +2,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
 
-class Network(tf.keras.Model):
-    def __init__(self,input_shape,units,activation,name):
+class StateNetwork(tf.keras.Model):
+    def __init__(self,units,activation,name):
         super().__init__(name=name)
 
         self.L = []
@@ -12,15 +12,21 @@ class Network(tf.keras.Model):
                                 activation=activation[i],
                                 name=f"L{i}"))
 
+    def _dummy_call(self,state_shape,action_dim):
         with tf.device("/cpu:0"):
-            self(tf.constant(np.zeros(input_shape, dtype=np.float32)))
+            self(tf.constant(np.zeros((1,)+state_shape, dtype=np.float32)))
 
     def call(self, inputs):
         for l in self.L:
             inputs = l(inputs)
         return inputs
 
-class StateActionNetwork(Network):
+class StateActionNetwork(StateNetwork):
+    def _dummy_call(self,state_shape,action_dim):
+        with tf.device("/cpu:0"):
+            self(tf.constant(np.zeros((1,)+state_shape, dtype=np.float32)),
+                 tf.constant(np.zeros([1, action_dim], dtype=np.float32)))
+
     def call(self, inputs):
         states, action = inputs
         return super().call(tf.concat([states, action], axis=1))
