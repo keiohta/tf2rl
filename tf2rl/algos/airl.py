741aac235e59c8871fc061bd3d04ab2ec510aafc
Kei Ohta <dev.ohtakei@gmail.com>
2019-12-20 21:23:50 +0900

    Fix a bug of missing member in test_airl

--- tf2rl/algos/airl.py
+++ tf2rl/algos/airl.py
@@ -3,14 +3,15 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense
 
 from tf2rl.algos.policy_base import IRLPolicy
+from tf2rl.algos.gail import Discriminator as StateActionModel
 from tf2rl.networks.spectral_norm_dense import SNDense
 
 
-class BaseModel(tf.keras.Model):
-    def __init__(self, state_shape, action_dim=None, units=[32, 32],
+class StateModel(tf.keras.Model):
+    def __init__(self, state_shape, units=[32, 32],
                  enable_sn=False, output_activation="sigmoid",
                  name="Discriminator"):
-        super().__init__(self, name=name)
+        super().__init__(name=name)
 
         DenseClass = SNDense if enable_sn else Dense
         self.l1 = DenseClass(units[0], name="L1", activation="relu")
@@ -19,37 +20,8 @@ class BaseModel(tf.keras.Model):
 
         dummy_state = tf.constant(
             np.zeros(shape=(1,)+state_shape, dtype=np.float32))
-        if action_dim is None:
-            dummy_inputs = [dummy_state]
-        else:
-            dummy_action = tf.constant(
-                np.zeros(shape=[1, action_dim], dtype=np.float32))
-            dummy_inputs = [dummy_state, dummy_action]
-        self._define_shape(dummy_inputs)
-
-    def _define_shape(self, dummy_inputs):
         with tf.device("/cpu:0"):
-            self(dummy_inputs)
-
-    def call(self, inputs):
-        features = tf.concat(inputs, axis=1)
-        features = self.l1(features)
-        features = self.l2(features)
-        return self.l3(features)
-
-
-class RewardApproximator(BaseModel):
-    def __init__(self, *, name="RewardApprox", **kwargs):
-        super().__init__(name=name, **kwargs)
-
-
-class ValueApproximator(BaseModel):
-    def __init__(self, *, name="ValueApproximator", action_dim=None, **kwargs):
-        super().__init__(name=name, action_dim=action_dim, **kwargs)
-
-    def _define_shape(self, dummy_inputs):
-        with tf.device("/cpu:0"):
-            self(dummy_inputs[0])
+            self(dummy_state)
 
     def call(self, inputs):
         features = self.l1(inputs)
@@ -61,7 +33,7 @@ class AIRL(IRLPolicy):
     def __init__(
             self,
             state_shape,
-            action_dim=None,
+            action_dim,
             state_only=True,
             units=[32, 32],
             lr=0.001,
@@ -71,12 +43,16 @@ class AIRL(IRLPolicy):
         super().__init__(name=name, n_training=1, **kwargs)
         self._state_only = state_only
         if state_only:
-            action_dim = None
-        self.rew_net = RewardApproximator(
-            state_shape=state_shape, action_dim=action_dim,
-            units=units, enable_sn=enable_sn)
-        self.val_net = ValueApproximator(
-            state_shape=state_shape, units=units, enable_sn=enable_sn)
+            self.rew_net = StateModel(
+                state_shape=state_shape, units=units,
+                name="reward_net", enable_sn=enable_sn, output_activation="linear")
+        else:
+            self.rew_net = StateActionModel(
+                state_shape=state_shape, action_dim=action_dim, units=units,
+                name="reward_net", enable_sn=enable_sn, output_activation="linear")
+        self.val_net = StateModel(
+            state_shape=state_shape, units=units, enable_sn=enable_sn,
+            name="val_net", output_activation="linear")
         self.rew_optimizer = tf.keras.optimizers.Adam(
             learning_rate=lr)
         self.val_optimizer = tf.keras.optimizers.Adam(
@@ -95,8 +71,8 @@ class AIRL(IRLPolicy):
         with tf.device(self.device):
             with tf.GradientTape(persistent=True) as tape:
                 if self._state_only:
-                    real_rews = self.rew_net([expert_states])
-                    fake_rews = self.rew_net([agent_states])
+                    real_rews = self.rew_net(expert_states)
+                    fake_rews = self.rew_net(agent_states)
                 else:
                     real_rews = self.rew_net([expert_states, expert_acts])
                     fake_rews = self.rew_net([agent_states, agent_acts])
@@ -127,7 +103,7 @@ class AIRL(IRLPolicy):
     def _inference_body(self, states, actions):
         with tf.device(self.device):
             if self._state_only:
-                return self.rew_net([states])
+                return self.rew_net(states)
             else:
                 return self.rew_net([states, actions])
 
