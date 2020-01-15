cd9f21c9eed112c6a5bc3b766757c2f2cfbeff98
Kei Ohta <dev.ohtakei@gmail.com>
2020-01-15 20:24:25 +0900

    Add debug setting for run_ppo.py

--- examples/run_ppo.py
+++ examples/run_ppo.py
@@ -1,7 +1,6 @@
 import gym
 
 from tf2rl.algos.ppo import PPO
-from tf2rl.policies.categorical_actor import CategoricalActorCritic
 from tf2rl.experiments.on_policy_trainer import OnPolicyTrainer
 from tf2rl.envs.utils import is_discrete, get_act_dim
 
@@ -12,7 +11,8 @@ if __name__ == '__main__':
     parser.add_argument('--env-name', type=str,
                         default="Pendulum-v0")
     parser.set_defaults(test_interval=20480)
-    parser.set_defaults(max_steps=int(1e7))
+    parser.set_defaults(save_summary_interval=20480)
+    parser.set_defaults(max_steps=int(2e6))
     parser.set_defaults(horizon=2048)
     parser.set_defaults(batch_size=64)
     parser.set_defaults(gpu=-1)
@@ -21,6 +21,16 @@ if __name__ == '__main__':
     env = gym.make(args.env_name)
     test_env = gym.make(args.env_name)
 
+    # DEBUG
+    args.normalize_adv = True
+    args.enable_gae = True
+    if args.env_name == "Pendulum-v0":
+        discount = 0.9
+        lam = 0.95
+    else:
+        discount = 0.995
+        lam = 0.97
+
     policy = PPO(
         state_shape=env.observation_space.shape,
         action_dim=get_act_dim(env.action_space),
@@ -28,13 +38,15 @@ if __name__ == '__main__':
         max_action=None if is_discrete(
             env.action_space) else env.action_space.high[0],
         batch_size=args.batch_size,
-        actor_units=[64, 64],
-        critic_units=[64, 64],
+        actor_units=(64, 64),
+        critic_units=(64, 64),
         n_epoch=10,
         lr_actor=3e-4,
         lr_critic=3e-4,
-        discount=0.995,
-        lam=0.97,
+        # discount=0.995,
+        # lam=0.97,
+        discount=discount,
+        lam=lam,
         horizon=args.horizon,
         normalize_adv=args.normalize_adv,
         enable_gae=args.enable_gae,
