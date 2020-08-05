182b43481207f52900f9df7bdbdd17915310056c
Yamada <ymd_h@outlook.jp>
2020-08-05 23:13:42 +0900

    Remove trailing white space (W291)

--- tf2rl/misc/discount_cumsum.py
+++ tf2rl/misc/discount_cumsum.py
@@ -8,7 +8,7 @@ def discount_cumsum(x, discount):
     :param x (np.ndarray or tf.Tensor)
         vector of [x0, x1, x2]
     :return output:
-        [x0 + discount * x1 + discount^2 * x2,  
+        [x0 + discount * x1 + discount^2 * x2,
          x1 + discount * x2,
          x2]
     """
