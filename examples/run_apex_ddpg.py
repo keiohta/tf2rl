3321da2b461dee47ece10bed38a11653b1e76eac
Yamada <ymd_h@outlook.jp>
2020-08-06 16:02:18 +0900

    Define mock functions at top level for Windows multiprocessing

--- examples/run_apex_ddpg.py
+++ examples/run_apex_ddpg.py
@@ -5,6 +5,48 @@ from tf2rl.algos.ddpg import DDPG
 from tf2rl.misc.target_update_ops import update_target_variables
 
 
+# Prepare env and policy function
+class env_fn:
+    def __init__(self,env_name):
+        self.env_name = env_name
+
+    def __call__(self):
+        return gym.make(self.env_name)
+
+def policy_fn(env, name, memory_capacity=int(1e6),
+              gpu=-1, noise_level=0.3):
+    return DDPG(
+        state_shape=env.observation_space.shape,
+        action_dim=env.action_space.high.size,
+        max_action=env.action_space.high[0],
+        gpu=gpu,
+        name=name,
+        sigma=noise_level,
+        batch_size=100,
+        lr_actor=0.001,
+        lr_critic=0.001,
+        actor_units=[400, 300],
+        critic_units=[400, 300],
+        memory_capacity=memory_capacity)
+
+def get_weights_fn(policy):
+    # TODO: Check if following needed
+    import tensorflow as tf
+    with tf.device(policy.device):
+        return [policy.actor.weights,
+                policy.critic.weights,
+                policy.critic_target.weights]
+
+def set_weights_fn(policy, weights):
+    actor_weights, critic_weights, critic_target_weights = weights
+    update_target_variables(
+        policy.actor.weights, actor_weights, tau=1.)
+    update_target_variables(
+        policy.critic.weights, critic_weights, tau=1.)
+    update_target_variables(
+        policy.critic_target.weights, critic_target_weights, tau=1.)
+
+
 if __name__ == '__main__':
     parser = apex_argument()
     parser.add_argument('--env-name', type=str,
@@ -12,41 +54,4 @@ if __name__ == '__main__':
     parser = DDPG.get_argument(parser)
     args = parser.parse_args()
 
-    # Prepare env and policy function
-    def env_fn():
-        return gym.make(args.env_name)
-
-    def policy_fn(env, name, memory_capacity=int(1e6),
-                  gpu=-1, noise_level=0.3):
-        return DDPG(
-            state_shape=env.observation_space.shape,
-            action_dim=env.action_space.high.size,
-            max_action=env.action_space.high[0],
-            gpu=gpu,
-            name=name,
-            sigma=noise_level,
-            batch_size=100,
-            lr_actor=0.001,
-            lr_critic=0.001,
-            actor_units=[400, 300],
-            critic_units=[400, 300],
-            memory_capacity=memory_capacity)
-
-    def get_weights_fn(policy):
-        # TODO: Check if following needed
-        import tensorflow as tf
-        with tf.device(policy.device):
-            return [policy.actor.weights,
-                    policy.critic.weights,
-                    policy.critic_target.weights]
-
-    def set_weights_fn(policy, weights):
-        actor_weights, critic_weights, critic_target_weights = weights
-        update_target_variables(
-            policy.actor.weights, actor_weights, tau=1.)
-        update_target_variables(
-            policy.critic.weights, critic_weights, tau=1.)
-        update_target_variables(
-            policy.critic_target.weights, critic_target_weights, tau=1.)
-
-    run(args, env_fn, policy_fn, get_weights_fn, set_weights_fn)
+    run(args, env_fn(args.env_name), policy_fn, get_weights_fn, set_weights_fn)
--- tests/algos/test_apex.py
+++ tests/algos/test_apex.py
@@ -26,33 +26,8 @@ class TestApeX(unittest.TestCase):
         parser.set_defaults(n_warmup=1)
         args, _ = parser.parse_known_args()
 
-        def env_fn():
-            return gym.make("CartPole-v0")
-
-        def policy_fn(env, name, memory_capacity=int(1e6), gpu=-1, *args, **kwargs):
-            return DQN(
-                name=name,
-                state_shape=env.observation_space.shape,
-                action_dim=env.action_space.n,
-                n_warmup=500,
-                target_replace_interval=300,
-                batch_size=32,
-                memory_capacity=memory_capacity,
-                discount=0.99,
-                gpu=-1)
-
-        def get_weights_fn(policy):
-            return [policy.q_func.weights,
-                    policy.q_func_target.weights]
-
-        def set_weights_fn(policy, weights):
-            q_func_weights, qfunc_target_weights = weights
-            update_target_variables(
-                policy.q_func.weights, q_func_weights, tau=1.)
-            update_target_variables(
-                policy.q_func_target.weights, qfunc_target_weights, tau=1.)
-
-        run(args, env_fn, policy_fn, get_weights_fn, set_weights_fn)
+        run(args, env_fn_discrete, policy_fn_discrete,
+            get_weights_fn_discrete, set_weights_fn_discrete)
 
     def test_run_continuous(self):
         from tf2rl.algos.ddpg import DDPG
@@ -60,32 +35,65 @@ class TestApeX(unittest.TestCase):
         parser.set_defaults(n_warmup=1)
         args, _ = parser.parse_known_args()
 
-        def env_fn():
-            return gym.make('Pendulum-v0')
+        run(args, env_fn_continuous, policy_fn_continuous,
+            get_weights_fn_continuous, set_weights_fn_continuous)
 
-        def policy_fn(env, name, memory_capacity=int(1e6), gpu=-1, *args, **kwargs):
-            return DDPG(
-                state_shape=env.observation_space.shape,
-                action_dim=env.action_space.high.size,
-                n_warmup=500,
-                gpu=-1)
 
-        def get_weights_fn(policy):
-            return [policy.actor.weights,
-                    policy.critic.weights,
-                    policy.critic_target.weights]
+def env_fn_discrete():
+    return gym.make("CartPole-v0")
 
-        def set_weights_fn(policy, weights):
-            actor_weights, critic_weights, critic_target_weights = weights
-            update_target_variables(
-                policy.actor.weights, actor_weights, tau=1.)
-            update_target_variables(
-                policy.critic.weights, critic_weights, tau=1.)
-            update_target_variables(
-                policy.critic_target.weights, critic_target_weights, tau=1.)
+def policy_fn_discrete(env, name, memory_capacity=int(1e6), gpu=-1, *args, **kwargs):
+    from tf2rl.algos.dqn import DQN
+    return DQN(
+        name=name,
+        state_shape=env.observation_space.shape,
+        action_dim=env.action_space.n,
+        n_warmup=500,
+        target_replace_interval=300,
+        batch_size=32,
+        memory_capacity=memory_capacity,
+        discount=0.99,
+        gpu=-1)
 
-        run(args, env_fn, policy_fn, get_weights_fn, set_weights_fn)
+def get_weights_fn_discrete(policy):
+    return [policy.q_func.weights,
+            policy.q_func_target.weights]
 
+def set_weights_fn_discrete(policy, weights):
+    q_func_weights, qfunc_target_weights = weights
+    update_target_variables(
+        policy.q_func.weights, q_func_weights, tau=1.)
+    update_target_variables(
+        policy.q_func_target.weights, qfunc_target_weights, tau=1.)
+
+
+def env_fn_continuous():
+    return gym.make('Pendulum-v0')
+
+
+def policy_fn_continuous(env, name, memory_capacity=int(1e6), gpu=-1, *args, **kwargs):
+    from tf2rl.algos.ddpg import DDPG
+    return DDPG(
+        state_shape=env.observation_space.shape,
+        action_dim=env.action_space.high.size,
+        n_warmup=500,
+        gpu=-1)
+
+
+def get_weights_fn_continuous(policy):
+    return [policy.actor.weights,
+            policy.critic.weights,
+            policy.critic_target.weights]
+
+
+def set_weights_fn_continuous(policy, weights):
+    actor_weights, critic_weights, critic_target_weights = weights
+    update_target_variables(
+        policy.actor.weights, actor_weights, tau=1.)
+    update_target_variables(
+        policy.critic.weights, critic_weights, tau=1.)
+    update_target_variables(
+        policy.critic_target.weights, critic_target_weights, tau=1.)
 
 if __name__ == '__main__':
     unittest.main()
