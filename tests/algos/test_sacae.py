2f6e239f86ebf275035c653b827164ef3b9a2e20
Kei Ohta <dev.ohtakei@gmail.com>
2021-06-14 18:56:14 +0900

    test: add unittests for SACAE and CURL

--- tests/algos/test_sacae.py
+++ tests/algos/test_sacae.py
@@ -1,14 +1,14 @@
 import unittest
 
-from tf2rl.algos.curl_sac import CURL
+from tf2rl.algos.sac_ae import SACAE
 from tests.algos.common import CommonOffPolImgContinuousAlgos
 
 
 class TestSAC(CommonOffPolImgContinuousAlgos):
     @classmethod
     def setUpClass(cls):
-        super().setUpClass(img_dim=100)
-        cls.agent = CURL(
+        super().setUpClass(img_dim=84)
+        cls.agent = SACAE(
             action_dim=cls.continuous_env.action_space.low.size,
             batch_size=cls.batch_size,
             gpu=-1)
