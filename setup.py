0f5943022b22e5cf9167501a0fc5fac428eaf768
Kei Ohta <dev.ohtakei@gmail.com>
2021-06-14 19:52:38 +0900

    env: fix install dependencies

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ compatible_tfa = {"2.3": ["tensorflow_addons~=0.13.0"],
                   "2.1": ["tensorflow_addons~=0.9.1"],
                   "2.0": ["tensorflow_addons~=0.6.0"]}
 install_requires.append(*compatible_tfp[tf_version])
-if not (platform.system() == 'Windows') and (tf.version.VERSION.rsplit('.', 1)[0] == "2.0"):
+if not (platform.system() == 'Windows' and tf_version == "2.0"):
     # tensorflow-addons does not support tf2.0 on Windows
     install_requires.append(*compatible_tfa[tf_version])
 
