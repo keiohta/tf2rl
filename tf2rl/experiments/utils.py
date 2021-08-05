9c7b87b19a9f9f166f27bce93cdbe39199045886
Yamada <ymd_h@outlook.jp>
2021-08-05 12:40:25 +0900

    Fix #144: Pass ignored `max_steps`

--- tf2rl/experiments/utils.py
+++ tf2rl/experiments/utils.py
@@ -13,7 +13,7 @@ def save_path(samples, filename):
 def restore_latest_n_traj(dirname, n_path=10, max_steps=None):
     assert os.path.isdir(dirname)
     filenames = get_filenames(dirname, n_path)
-    return load_trajectories(filenames, None)
+    return load_trajectories(filenames, max_steps)
 
 
 def get_filenames(dirname, n_path=None):
