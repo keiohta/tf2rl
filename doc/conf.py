c36d0a00d3e5dbfb001d56a520f05b7d7f5b7a20
Yamada <ymd_h@outlook.jp>
2021-10-30 21:20:22 +0900

    Sort document by source

--- doc/conf.py
+++ doc/conf.py
@@ -9,6 +9,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "recommonmark"]
 html_theme = "sphinx_rtd_theme"
 
 autoclass_content = "both"
+autodoc_member_order = "bysource"
 
 def setup(app):
     app.add_transform(AutoStructify)
